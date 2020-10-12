910e9adc650ca5c79859d8c62ec579ca58baa649
塩谷友浩 <68536993+satsukiTomo@users.noreply.github.com>
2020-10-12 15:18:54 +0900

    牛歩カードの仕様変更

--- game/object/map/print/frames/map/PlayFrame.java
+++ game/object/map/print/frames/map/PlayFrame.java
@@ -157,7 +157,11 @@ public class PlayFrame extends FrameModel{
 	public void reloadInfo() {
 		mainInfo.setVisible(false);
 		if(ContainsEvent.isEffect()){
-			mainInfo.setText("自社情報　"+"名前："+Player.player.getName()+"　持ち金："+FrameEvent.convertMoney(Player.player.getMoney())+"　"+App.year+"年目　"+App.month+"月　"+Japan.getGoalName()+"まで"+Player.player.getGoalDistance()+"マス　効果発動中("+Player.player.getEffect()+")");
+			if(Player.player.getEffect()==-3) {
+				mainInfo.setText("自社情報 "+"名前："+Player.player.getName()+" 持ち金："+FrameEvent.convertMoney(Player.player.getMoney())+" "+App.year+"年目 "+App.month+"月 "+Japan.getGoalName()+"まで"+Player.player.getGoalDistance()+"マス 牛歩カード効果発動中");
+			}else {
+				mainInfo.setText("自社情報　"+"名前："+Player.player.getName()+"　持ち金："+FrameEvent.convertMoney(Player.player.getMoney())+"　"+App.year+"年目　"+App.month+"月　"+Japan.getGoalName()+"まで"+Player.player.getGoalDistance()+"マス　効果発動中("+Player.player.getEffect()+")");
+			}
 		}else {
 			mainInfo.setText("自社情報　"+"名前："+Player.player.getName()+"　持ち金："+FrameEvent.convertMoney(Player.player.getMoney())+"　"+App.year+"年目　"+App.month+"月　"+Japan.getGoalName()+"まで"+Player.player.getGoalDistance()+"マス");
 		}
